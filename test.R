d5e0cc71820398588c00ac3cee6a0dcbf0a7e38b
Curtis <ecurtis@mariners.com>
2021-03-26 11:31:19 -0700

    add test file

---
+++ test.R
@@ -0,0 +1,19 @@
+# OPENXLSX INITIALIZATIONS ------------------------------------------
+pacman::p_load(openxlsx)
+pacman::p_load(lubridate)
+pacman::p_load(runner)
+pacman::p_load(googledrive)
+pacman::p_load(googlesheets4)
+
+start_time = Sys.time()
+
+extrafont::loadfonts(device = "win",
+                     quiet = T)
+
+pacman::p_load(tidyverse)
+
+windowsFonts(header_font = windowsFont('Dinot-CondBlackIta'))
+windowsFonts(subheader_font = windowsFont('DINOT-CondBoldIta'))
+windowsFonts(subheader_font2 = windowsFont('Dinot-CondMediIta'))
+windowsFonts(body_font = windowsFont('Dinot-CondBold'))
+windowsFonts(body_font2 = windowsFont('Dinot-CondMedium'))
